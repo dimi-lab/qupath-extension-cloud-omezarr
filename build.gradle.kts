522d8a7a685cff5bdc9d0b94d7a0dc2b0e105124
David Haley <dchaley@gmail.com>
2025-03-06 22:03:10 -0800

    Reformat build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,32 +1,32 @@
 plugins {
-    // To optionally create a shadow/fat jar that bundle up any non-core dependencies
-    id("com.gradleup.shadow") version "8.3.5"
-    // QuPath Gradle extension convention plugin
-    id("qupath-conventions")
+  // To optionally create a shadow/fat jar that bundle up any non-core dependencies
+  id("com.gradleup.shadow") version "8.3.5"
+  // QuPath Gradle extension convention plugin
+  id("qupath-conventions")
 }
 
 // TODO: Configure your extension here (please change the defaults!)
 qupathExtension {
-    name = "qupath-extension-cloud-omezarr"
-    group = "io.github.dimilab"
-    version = "0.1.0-SNAPSHOT"
-    description = "QuPath extension to load OME-Zarr images from cloud storage"
-    automaticModule = "io.github.dimi-lab.qupath.extension.cloud-omezarr"
+  name = "qupath-extension-cloud-omezarr"
+  group = "io.github.dimilab"
+  version = "0.1.0-SNAPSHOT"
+  description = "QuPath extension to load OME-Zarr images from cloud storage"
+  automaticModule = "io.github.dimi-lab.qupath.extension.cloud-omezarr"
 }
 
 // TODO: Define your dependencies here
 dependencies {
 
-    // Main dependencies for most QuPath extensions
-    shadow(libs.bundles.qupath)
-    shadow(libs.bundles.logging)
-    shadow(libs.qupath.fxtras)
+  // Main dependencies for most QuPath extensions
+  shadow(libs.bundles.qupath)
+  shadow(libs.bundles.logging)
+  shadow(libs.qupath.fxtras)
 
-    // If you aren't using Groovy, this can be removed
-    shadow(libs.bundles.groovy)
+  // If you aren't using Groovy, this can be removed
+  shadow(libs.bundles.groovy)
 
-    // For testing
-    testImplementation(libs.bundles.qupath)
-    testImplementation(libs.junit)
+  // For testing
+  testImplementation(libs.bundles.qupath)
+  testImplementation(libs.junit)
 
 }
